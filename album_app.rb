698b6adca926b7831eeb3f535a0618f37df93b19
Krishan <krishanchopra30@yahoo.com>
2018-02-14 14:57:06 -0800

    Add load basic output in table

--- album_app.rb
+++ album_app.rb
@@ -1,13 +1,31 @@
+require 'csv'
+
 class AlbumApp
 
   def call(env)
     response_body = "<h1>Top 100 Albums of All Time</h1>"
     # Read the data from the file.
-    albums = ['fee', 'fi', 'fo', 'funk']
-    # Append it to the response body.
-    response_body << albums.to_s
+    @albums = loadFromFile
+
+    response_body << "<table>"
+
+    @albums.each {|row| response_body << "<tr><td>#{row.to_s}</tr></td>"}
+  	
+    response_body << "</table>"
+
     # Send the response
     [200, {'Content-Type' => 'text/html'}, [response_body.to_s]]
+  end
+
+  def loadFromFile
+  	albums = []
+
+  	CSV.foreach("top_100_albums.txt") do |row|
+  		albums.push({ number: 1, title: row[0], year: row[1] })
+	end
+
+	albums
+
   end
 
 end
