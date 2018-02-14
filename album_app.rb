3696e5101e684f07ea843ea65e79b218e81dda11
Krishan <krishanchopra30@yahoo.com>
2018-02-14 15:22:35 -0800

    Fix naming to proper ruby format

--- album_app.rb
+++ album_app.rb
@@ -5,13 +5,13 @@ class AlbumApp
   def call(env)
     response_body = "<h1>Top 100 Albums of All Time</h1>"
     # Read the data from the file.
-    response_body << renderDataToTable
+    response_body << render_data_to_table(load_from_file)
 
     # Send the response
     [200, {'Content-Type' => 'text/html'}, [response_body.to_s]]
   end
 
-  def loadFromFile
+  def load_from_file
   	albums = []
 
   	CSV.foreach("top_100_albums.txt") do |row|
@@ -21,8 +21,8 @@ class AlbumApp
 	   albums
   end
 
-  def renderDataToTable
-    @albums = loadFromFile
+  def render_data_to_table(table_data)
+ 
     response_body = ""
 
     response_body << "<table>"
@@ -32,7 +32,7 @@ class AlbumApp
         response_body << "<th>Year</th>"        
       response_body << "</tr></thead>"
       response_body << "<tbody>"
-        @albums.each {|row| response_body << "<tr><td>#{row[:number]}</td><td>#{row[:title]}</td><td>#{row[:year]}</td></tr>"}
+        table_data.each {|row| response_body << "<tr><td>#{row[:number]}</td><td>#{row[:title]}</td><td>#{row[:year]}</td></tr>"}
       response_body << "</tbody>"
     response_body << "</table>"
 
