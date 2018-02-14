336760d21ed95b2649ce15c8f7b1094ec15c18d6
Krishan <krishanchopra30@yahoo.com>
2018-02-14 15:09:55 -0800

    Render the data to a table

--- album_app.rb
+++ album_app.rb
@@ -5,18 +5,7 @@ class AlbumApp
   def call(env)
     response_body = "<h1>Top 100 Albums of All Time</h1>"
     # Read the data from the file.
-    @albums = loadFromFile
-
-    response_body << "<table>"
-      response_body << "<thead><tr>"
-        response_body << "<th>Rank</th>"        
-        response_body << "<th>Name</th>"        
-        response_body << "<th>Year</th>"        
-      response_body << "</tr></thead>"
-      response_body << "<tbody>"
-        @albums.each {|row| response_body << "<tr><td>#{row.to_s}</td><td>#{row.to_s}</td><td>#{row.to_s}</td></tr>"}
-      response_body << "</tbody>"
-    response_body << "</table>"
+    response_body << renderDataToTable
 
     # Send the response
     [200, {'Content-Type' => 'text/html'}, [response_body.to_s]]
@@ -27,10 +16,29 @@ class AlbumApp
 
   	CSV.foreach("top_100_albums.txt") do |row|
   		albums.push({ number: 1, title: row[0], year: row[1] })
-	end
+	   end
 
-	albums
+	   albums
+  end
+
+  def renderDataToTable
+    @albums = loadFromFile
+    response_body = ""
+
+    response_body << "<table>"
+      response_body << "<thead><tr>"
+        response_body << "<th>Rank</th>"        
+        response_body << "<th>Name</th>"        
+        response_body << "<th>Year</th>"        
+      response_body << "</tr></thead>"
+      response_body << "<tbody>"
+        @albums.each {|row| response_body << "<tr><td>#{row[:number]}</td><td>#{row[:title]}</td><td>#{row[:year]}</td></tr>"}
+      response_body << "</tbody>"
+    response_body << "</table>"
+
+    response_body
 
   end
+
 
 end
