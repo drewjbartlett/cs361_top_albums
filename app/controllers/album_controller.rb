786802a48ef9ada09c775c3aa73500758df26f2f
Drew Bartlett <drewb9@gmail.com>
2018-02-13 22:35:08 -0800

    Add extra styling. Remove commented stylesheet. Add color to selected header link

--- app/controllers/album_controller.rb
+++ app/controllers/album_controller.rb
@@ -14,11 +14,17 @@ class AlbumController < BaseController
     @headers = [
       { sort_key: 'number', name: '#' },
       { sort_key: 'name', name: 'Name' },
-      { sort_key: 'year', name: 'Near' },
+      { sort_key: 'year', name: 'Year' },
     ]
 
     @highlighted_index = (@request.params.has_key?('highlighted_index') ? @request.params['highlighted_index'] : nil).to_i
+    @sorted_by = (@request.params.has_key?('sort_by') ? @request.params['sort_by'] : nil)
 
     self.render('albums')
+  end
+
+  def sort_by_link(sort_key)
+    sort_by_link = "?sort_by=#{sort_key}"
+    sort_by_link + (@highlighted_index != 0 ? "&highlighted_index=#{@highlighted_index}" : '')
   end
 end
