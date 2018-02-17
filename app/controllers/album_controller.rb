5434a555c2f74cfd7ad9c853b8098b824a11a9c5
Drew Bartlett <drewb9@gmail.com>
2018-02-16 18:29:33 -0800

    Add loop for albums in erb file

--- app/controllers/album_controller.rb
+++ app/controllers/album_controller.rb
@@ -6,14 +6,12 @@ class AlbumController < BaseController
 	def view_albums
 		# Read the data from the file.
     	album_model = Model.new("top_100_albums.txt")
-    	table_data = album_model.load_from_file
+    	@table_data = album_model.load_from_file
 
     	if (@request.params.has_key?('sort_by'))
-      		puts "sorting"
-      		table_data = album_model.sort_data_by_key(@request.params['sort_by'], table_data)
+      	@table_data = album_model.sort_data_by_key(@request.params['sort_by'], @table_data)
     	end
 
-	    #response_body << render_data_to_table(table_data)
 	    response(200, render_erb_file('app/views/album.html.erb'))
 	end
 
--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,3 +1,4 @@
+require 'erb'
 
 class BaseController
 
