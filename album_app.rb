6b3a6702f10cec2f6f3a01f2b22880ca3d7aee0a
Drew Bartlett <drewb9@gmail.com>
2018-02-18 15:06:48 -0800

    Remove stale code for rendering table data. This now lives in the erb file. Fixes #2.

--- album_app.rb
+++ album_app.rb
@@ -12,23 +12,4 @@ class AlbumApp
 
   end
 
-  def render_data_to_table(table_data)
- 
-    response_body = ""
-
-    response_body << "<table>"
-      response_body << "<thead><tr>"
-        response_body << '<th><a href="?sort_by=number">Rank</a></th>'        
-        response_body << '<th><a href="?sort_by=title">Name</a></th>'        
-        response_body << '<th><a href="?sort_by=year">Year</a></th>'            
-      response_body << "</tr></thead>"
-      response_body << "<tbody>"
-        table_data.each {|row| response_body << "<tr><td>#{row[:number]}</td><td>#{row[:title]}</td><td>#{row[:year]}</td></tr>"}
-      response_body << "</tbody>"
-    response_body << "</table>"
-
-    response_body
-
-  end
-
 end
