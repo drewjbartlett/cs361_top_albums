173225280c218a784d58780f88c9ba726055656d
Krishan <krishanchopra30@yahoo.com>
2018-02-26 15:10:47 -0800

    Refactor load_from_file.

--- app/utils/album_loader.rb
+++ app/utils/album_loader.rb
@@ -5,13 +5,11 @@ require_relative '../models/album_model'
 module AlbumLoader
 
   def self.load_from_file(file_name)
-    data = []
-
-    CSV.foreach(Paths::data_path(file_name)) do |row|
-      data.push(AlbumModel.new($., row[0], row[1]))
+    [].tap do |albums|
+      CSV.foreach(Paths::data_path(file_name)) do |row|
+        albums << AlbumModel.new($., row[0], row[1])
+      end
     end
-
-    data
   end
 
 end
