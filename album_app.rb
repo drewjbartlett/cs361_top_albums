9049f2e2827f46e63abddb74db08ed215dcdde22
Krishan <krishanchopra30@yahoo.com>
2018-02-28 07:32:04 -0800

    Attempting to get the sort methods working using sinatra - still not implemented

--- album_app.rb
+++ album_app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require_relative 'app/collections/album_collection'
-#require_relative 'app/controllers/album_controller'
+require 'erb'
+
 
 set :views, Proc.new {File.join(root, 'app/views')}
 
@@ -9,3 +10,22 @@ get '/' do
   erb :index
 end
 
+get '/rank' do
+  @album_collection = AlbumCollection.new('top_100_albums.txt')
+  erb :index
+end
+
+get '/title' do
+  @album_collection = AlbumCollection.new('top_100_albums.txt')
+  erb :index
+end
+
+get '/year' do
+  @album_collection = AlbumCollection.new('top_100_albums.txt')
+  erb :index
+end
+
+post '/highlight' do
+  @album_collection = AlbumCollection.new('top_100_albums.txt')
+  erb :index
+end
