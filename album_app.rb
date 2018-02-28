e310fa970cfd5e33614220726f0a8e6d3c9f2740
Krishan <krishanchopra30@yahoo.com>
2018-02-28 10:35:02 -0800

    Cleaned up syntax issue. Sorting methods still aren't functioning

--- album_app.rb
+++ album_app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require_relative 'app/collections/album_collection'
+#require_relative 'app/controllers/album_controller'
 require 'erb'
 
 
@@ -7,25 +8,30 @@ set :views, Proc.new {File.join(root, 'app/views')}
 
 get '/' do
   @album_collection = AlbumCollection.new('top_100_albums.txt')
+  #@album_collection.sort_data_by_key(@request.params['sort_by'], @albums)
   erb :index
 end
 
 get '/rank' do
   @album_collection = AlbumCollection.new('top_100_albums.txt')
+  #@albums = album_collection.sort_data_by_key(@request.params['sort_by'], @albums)
   erb :index
 end
 
 get '/title' do
   @album_collection = AlbumCollection.new('top_100_albums.txt')
+ # @albums = album_collection.sort_data_by_key(@request.params['sort_by'], @albums)
   erb :index
 end
 
 get '/year' do
   @album_collection = AlbumCollection.new('top_100_albums.txt')
+ # @albums = album_collection.sort_data_by_key(@request.params['sort_by'], @albums)
   erb :index
 end
 
 post '/highlight' do
   @album_collection = AlbumCollection.new('top_100_albums.txt')
+  #@albums = album_collection.sort_data_by_key(@request.params['sort_by'], @albums)
   erb :index
 end
